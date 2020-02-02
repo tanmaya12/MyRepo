38a12221eb2c70ce6806bceebf4822d96636e661
Tanmaya Kumar Behera <46993425+tanmaya12@users.noreply.github.com>
2020-02-03 00:19:42 +0530

    in 6 decimal

--- JavaPractice/src/array/Decimal6Pos.java
+++ JavaPractice/src/array/Decimal6Pos.java
@@ -1,8 +1,18 @@
 package array;
 
+import java.text.DecimalFormat;
+
 public class Decimal6Pos {
 
 	public static void main(String[] args) {
+		double d;
+		d=(double)22/7;
+		System.out.println(d);
+		
+		//print upto decimal 6 digits
+		DecimalFormat df= new DecimalFormat("0.000000");
+		String format = df.format(d);
+		System.out.println(format);
 
 	}
 

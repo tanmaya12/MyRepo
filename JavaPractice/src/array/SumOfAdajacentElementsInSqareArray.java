eb97309d235d1fc3efe6c4a80ce7242a3ac499d4
Tanmaya Kumar Behera <46993425+tanmaya12@users.noreply.github.com>
2020-02-04 01:26:48 +0530

    Update SumOfAdajacentElementsInSqareArray.java

--- JavaPractice/src/array/SumOfAdajacentElementsInSqareArray.java
+++ JavaPractice/src/array/SumOfAdajacentElementsInSqareArray.java
@@ -34,8 +34,8 @@ public class SumOfAdajacentElementsInSqareArray {
 				//}
 			}
 		}
-		System.out.println("\n\t");	
-		System.out.println("\n\t");	
+		
+		
 		return sum-a[x][y]; //except the element
 	}
 

4becc37be10034c542f722bd48f7c4d1537a0f2c
Tanmaya Kumar Behera <46993425+tanmaya12@users.noreply.github.com>
2020-02-06 00:38:27 +0530

    from dev

---
+++ JavaPractice/src/java8/Try_Catch_Finally.java
@@ -0,0 +1,31 @@
+package java8;
+
+public class Try_Catch_Finally {
+
+	public static void main(String[] args) {
+		int a=6;
+		try {
+			a++;
+			System.out.println("try"+a);
+			for (int i = 0; i < 4; i++) {
+				int n=10/0;
+				continue;
+			}
+			//int n=10/0;
+			//continue;
+		} catch (Exception e) {
+			a++;
+			System.out.println("catch"+a);
+			a++;
+		}
+		catch (Throwable e) {
+			System.out.println("catch");
+		}		
+		finally 
+		{
+			System.out.println("final"+a);
+		}
+	
+	}
+
+}
---
+++ JavaPractice/src/java8/User_Exception.java
@@ -0,0 +1,22 @@
+package java8;
+
+class MyException extends Exception{
+
+	public MyException(String string) {
+		System.out.println("Ha haa haa");
+	}
+	
+}
+
+public class User_Exception {
+
+	public static void main(String[] args) throws MyException {
+		
+		if(true) {
+			System.out.println("main");
+			throw new MyException("throws work");
+		}
+
+	}
+
+}

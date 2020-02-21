dacfb0a09594b2a69b3538fdf5dc87bdc0f8a18f
Tanmaya Kumar Behera <46993425+tanmaya12@users.noreply.github.com>
2020-02-22 00:36:38 +0530

    tttt

--- JavaPractice/src/hackerrank/SumOfConsecutiveNumbersRange.java
+++ JavaPractice/src/hackerrank/SumOfConsecutiveNumbersRange.java
@@ -5,14 +5,16 @@ import java.util.stream.IntStream;
 public class SumOfConsecutiveNumbersRange {
 
 	public static void main(String[] args) {
-		int start=45, end= 259999;
-		long sum=0;
-		//long sum= (long) IntStream.range(start, end+1).sum();
-		for(long i=start; i<= end;i++) {
-			sum+= i;
+		int start = 45, end = 259999;
+		long sum = 0;
+		// long sum= (long) IntStream.range(start, end+1).sum();
+		for (long i = start; i <= end; i++) {
+			sum += i;
 		}
-		
+
 		System.out.println(sum);
+		long sum2 = (((end - start) + 1) / 2) * (start + end);
+		System.out.println(sum2);
 	}
 
 }
